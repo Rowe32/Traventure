f68130e669c931f973ac1c58644d6dbf60bd40c5
Rowena Schmidt <schmidt.rowena@gmail.com>
2022-03-21 11:45:12 +0100

    user model done

--- models/User.model.js
+++ models/User.model.js
@@ -1,16 +1,24 @@
 const { Schema, model } = require("mongoose");
 
-// TODO: Please make sure you edit the user model to whatever makes sense in this case
 const userSchema = new Schema(
   {
     username: {
       type: String,
-      // unique: true -> Ideally, should be unique, but its up to you
+      unique: true,
+      required: true,
     },
-    password: String,
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    travels: [{ type: Schema.Types.ObjectId, ref: 'Travel' }]
   },
   {
-    // this second object adds extra properties: `createdAt` and `updatedAt`
     timestamps: true,
   }
 );
@@ -18,12 +26,3 @@ const userSchema = new Schema(
 const User = model("User", userSchema);
 
 module.exports = User;
-
-
-User model
-
-```
-username: String
-email: String
-password: String
-travels: [{Travel-Objects}]
