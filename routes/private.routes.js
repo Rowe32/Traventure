b7cfbfae4bb66b43395a6e45c6eae01b7e620434
Vanessa <hello@vfernandes.dev>
2022-03-22 16:34:27 +0100

    Start for Editbtn

--- routes/private.routes.js
+++ routes/private.routes.js
@@ -1,56 +1,63 @@
 const router = require("express").Router();
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 const requireLogin = require("../middleware/routeGuard");
 const User = require("../models/User.model");
 const Travel = require("../models/Travel.model");
 
 router.use(requireLogin);
 
-
 router.get("/test", async (req, res) => {
-    const travel = await User.find().populate('travels');
-    console.log("Heyy its me", travel)
-    res.send("Send nothing")
-  })
+  const travel = await User.find().populate("travels");
+  console.log("Heyy its me", travel);
+  res.send("Send nothing");
+});
 router.get("/:username", (req, res) => {
-    //req.params.username
-    res.render('profile', { user : req.session.currentUser });
-})
+  //req.params.username
+  res.render("profile", { user: req.session.currentUser });
+});
 
 router.get("/:username/travels", async (req, res) => {
-    const owner = await User.findOne({username: req.params.username}).populate('travels');
-    const allTravelsOfOwner = owner.travels; //array of tavel objects
-    res.render('travelList', { user : req.session.currentUser, travels: allTravelsOfOwner} );
-})
+  const owner = await User.findOne({ username: req.params.username }).populate(
+    "travels"
+  );
+  const allTravelsOfOwner = owner.travels; //array of tavel objects
+  res.render("travelList", { user: req.session.currentUser, travels: allTravelsOfOwner });
+});
 
-router.get("/:username/travels/:id", (req, res) => {
-    res.render('travelDetails', { user : req.session.currentUser });
-})
+router.get("/:username/travels/:id", async (req, res) => {
+  const travelFromDb = await Travel.findOne({ _id: req.params.id });
+ console.log("Hi from line 29", req.params)
+  res.render("travelDetails", { user: req.session.currentUser, entry: travelFromDb });
+});
 
-router.post("/:username/travels/:id", async(req, res, next) => {
-    const {country, cities, dateStart, dateEnd} = req.body;
-    console.log("hello from line 39", dateStart.slice(0, 4));
-    const owner = await User.findOne({username: req.params.username})
-    const newTravel = {
-        owner,
-        country: country,
-        cities: cities,
-        dateStart: dateStart,
-        dateEnd: dateEnd,
-        year: Number(dateStart.slice(0, 4)),
-    }
-    await Travel.create(newTravel);
-    const travelFromDb = await Travel.findOne({country: country});
-    await User.findOneAndUpdate({username: req.params.username}, {$push: {travels: travelFromDb._id}})
-    res.redirect(`/private/${req.params.username}/travels`);
-})
+router.post("/:username/travels/:id", async (req, res, next) => {
+  const { country, cities, dateStart, dateEnd } = req.body;
+  console.log("hello from line 39", dateStart.slice(0, 4));
+  const owner = await User.findOne({ username: req.params.username });
+
+  const newTravel = {
+    owner,
+    country: country,
+    cities: cities,
+    dateStart: dateStart,
+    dateEnd: dateEnd,
+    year: Number(dateStart.slice(0, 4)),
+  };
+  await Travel.create(newTravel);
+  const travelFromDb = await Travel.findOne({ country: country });
+  await User.findOneAndUpdate(
+    { username: req.params.username },
+    { $push: { travels: travelFromDb._id } }
+  );
+  res.redirect(`/private/${req.params.username}/travels`);
+});
 
 router.post("/logout", (req, res) => {
-    req.session.destroy((err) => {
-      if (err) return next(err);
-  
-      res.redirect("/login");
-    })
-  })
+  req.session.destroy((err) => {
+    if (err) return next(err);
+
+    res.redirect("/login");
+  });
+});
 
 module.exports = router;
