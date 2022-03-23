adc9ebe238f35bb32f6a90a5ee5cee7578a37e28
Vanessa <hello@vfernandes.dev>
2022-03-23 11:09:16 +0100

    API in travelDetails

--- public/js/script.js
+++ public/js/script.js
@@ -6,23 +6,3 @@ document.addEventListener(
   false
 );
 
-const countryForInfobox = document.querySelector("#litograpthis");
-console.log("Hey line 10", countryForInfobox);
-
-//template literal for countes
-const endpoint = `https://en.wikipedia.org/w/api.php?action=parse&page=Spain&prop=text&formatversion=2&origin=*&format=json`;
-    
-async function getDataFromWikipedia() {
-  const response = await fetch(endpoint);
-  const data = await response.json();
-  
-  const wikiContainer = document.getElementById("wikiInfo");
- 
-  let myParser = new DOMParser()
-  let virtualDOM = myParser.parseFromString(data.parse.text, "text/html");
-  const infobox = virtualDOM.querySelector(".infobox");
-  wikiContainer.innerHTML = "";
-  wikiContainer.appendChild(infobox);
-}
-
-getDataFromWikipedia();
---
+++ public/js/wikiapi.js
@@ -0,0 +1,20 @@
+const countryForInfobox = document.querySelector("input#country");
+console.log("Hey line 10", countryForInfobox.value);
+
+//template literal for countes
+const endpoint = `https://en.wikipedia.org/w/api.php?action=parse&page=${countryForInfobox.value}&prop=text&formatversion=2&origin=*&format=json`;
+    
+async function getDataFromWikipedia() {
+  const response = await fetch(endpoint);
+  const data = await response.json();
+  
+  const wikiContainer = document.getElementById("wikiInfo");
+ 
+  let myParser = new DOMParser()
+  let virtualDOM = myParser.parseFromString(data.parse.text, "text/html");
+  const infobox = virtualDOM.querySelector(".infobox");
+  wikiContainer.innerHTML = "";
+  wikiContainer.appendChild(infobox);
+}
+
+getDataFromWikipedia();
