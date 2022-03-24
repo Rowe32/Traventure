7a182086249345713f62443b45c24dae93e4fe90
Rowena Schmidt <schmidt.rowena@gmail.com>
2022-03-24 15:03:07 +0100

    wiki infobox targetting

--- public/js/wikiapi.js
+++ public/js/wikiapi.js
@@ -2,22 +2,56 @@ const countryForInfobox = document.querySelector("#selectedCountry");
 
 //template literal for countes
 const endpoint = `https://en.wikipedia.org/w/api.php?action=parse&page=${countryForInfobox.value}&prop=text&formatversion=2&origin=*&format=json`;
-    
+
+let infobox;
+
 async function getDataFromWikipedia() {
   const response = await fetch(endpoint);
   const data = await response.json();
   
   const wikiContainer = document.getElementById("wikiInfo");
-  
-  // weiter runterbrechen: table tbody tr 
-  // population nur erster punkt!!
-  //currency
-  
+   
   let myParser = new DOMParser()
   let virtualDOM = myParser.parseFromString(data.parse.text, "text/html");
-  const infobox = virtualDOM.querySelector(".infobox");
+  infobox = virtualDOM.querySelector(".infobox");
+
+  setDisplayToNone()
+
   wikiContainer.innerHTML = "";
   wikiContainer.appendChild(infobox);
 }
 
 getDataFromWikipedia();
+
+
+// css selector [attribute~="value"]
+
+function setDisplayToNone() {
+  let coatOfArms = infobox.querySelectorAll('a')
+  Array.from(coatOfArms).forEach((elem) => { 
+    if (elem.innerHTML.trim() === "Coat of arms" || elem.innerHTML.trim() === "Emblem") {
+      elem.parentNode.parentNode.style.display = "none";
+    }
+  })
+
+  let anthem = infobox.querySelectorAll('b')
+  Array.from(anthem).forEach((elem) => { 
+    if (elem.innerHTML.toLowerCase() === "anthem:") {
+      elem.parentNode.style.display = "none";
+    }
+  })
+
+  let motto = infobox.querySelectorAll('b')
+  Array.from(motto).forEach((elem) => { 
+    console.log(elem.innerHTML)
+    if (elem.innerHTML.toLowerCase() === "motto:&nbsp;") {
+      elem.parentNode.style.display = "none";
+    }
+  })
+
+  let audio = infobox.querySelectorAll('audio')
+  Array.from(audio).forEach((elem) => { 
+   elem.parentNode.parentNode.parentNode.style.display = "none";
+  })
+  
+}
