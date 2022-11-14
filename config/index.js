8bba3416c944b872b57bd7766a833209c9e76499
Rowe Schmidt <90282261+Rowe32@users.noreply.github.com>
2022-11-14 20:15:21 +0100

    Update index.js

--- config/index.js
+++ config/index.js
@@ -30,9 +30,9 @@ module.exports = (app) => {
       resave: false,
       saveUninitialized: false,
       cookie: {
-        sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
-        secure: process.env.NODE_ENV === "production",
-        httpOnly: true,
+        //sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+        //secure: process.env.NODE_ENV === "production",
+        //httpOnly: true,
         maxAge: 2592000 // 60 * 1000 ms === 1 min
       },
   }));
